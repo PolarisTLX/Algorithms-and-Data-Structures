e9a6e36e189c289e713a6d7b2262cd1a4673c6be
PolarisTLX <paul.a.rail@gmail.com>
2018-03-19 14:09:52 -0400

    Making first commit on this Computer Sciences topic

---
+++ Types of algorithms.js
@@ -0,0 +1,21 @@
+/*
+Top 7 algorithms and data structures every programmer
+
+1. Sort Algorithms
+2. Search Algorithms
+3. Hashing
+4. Dynamic Programming
+5. Exponentiation by squaring
+6. String Matching and Parsing
+7. Primality Testing Algorithms
+
+
+
+Selection Sorting:
+
+Re-order an array of numbers to be in ascending order.
+Do this by finding smallest number and pushing it to new array,
+repeat n times where n = length of original array.
+
+
+*/
