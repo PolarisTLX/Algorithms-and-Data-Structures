f24c76b33211c5690f483c5cce122b1a7e082cb5
PolarisTLX <paul.a.rail@gmail.com>
2018-05-05 23:47:49 -0400

    simple Quick Sort algorithm

---
+++ Learneroo/Faster Sorting - Simple Counting Sort.rb
@@ -0,0 +1,62 @@
+https://www.learneroo.com/modules/4/nodes/16
+
+THIS IS MUCH SIMPLER AND FASTER THAN INSERTION SORT!!!
+
+Compare to previous exercise to full understanding:  https://www.learneroo.com/modules/4/nodes/15
+
+"In a simple sort, the only values that matter are the ones you are sorting. They are not keys to any other values. This makes counting sort very simple. If you already counted the values in the list, you don’t need to access the original list again. This challenge involves a simple counting sort where the elements being sorted are all that matter."
+
+
+
+def do_stuff(ar)
+	max = ar.max
+	min = 0
+	i = 0
+
+	while i <= max do
+		count = 0
+		ar.each do |element|
+			print element, " " if element == i
+		end
+		i += 1
+	end
+
+end
+
+t = gets.to_i
+for i in 1..t do
+		n=gets
+		ar = gets.strip.split.map {|i| i.to_i}
+		do_stuff(ar)
+		print "\n"
+end
+
+
+
+Input	Correct Output	Your Output
+
+63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33
+
+1 1 3 3 6 8 9 9 10 12 13 16 16 18 20 21 21 22 23 24 25 25 25 27 27 30 30 32 32 32 33 33 33 34 39 39 40 40 41 42 43 44 44 46 46 48 50 53 56 56 57 59 60 61 63 65 67 67 68 69 69 69 70 70 73 73 74 75 75 76 78 78 79 79 80 81 81 82 83 83 84 85 86 86 87 87 89 89 89 90 90 91 92 94 95 96 98 98 99 99
+
+1 1 3 3 6 8 9 9 10 12 13 16 16 18 20 21 21 22 23 24 25 25 25 27 27 30 30 32 32 32 33 33 33 34 39 39 40 40 41 42 43 44 44 46 46 48 50 53 56 56 57 59 60 61 63 65 67 67 68 69 69 69 70 70 73 73 74 75 75 76 78 78 79 79 80 81 81 82 83 83 84 85 86 86 87 87 89 89 89 90 90 91 92 94 95 96 98 98 99 99
+
+
+PREVIOUS EXERCISE WAS TO JUST COUNT NUMBER OF INSTANCES AS YOU RAN THROUGH IT
+PREVIOUS EXERCISE CODE IS ONLY DIFFERENT BY 2 LINES:
+
+while i <= max do
+  count = 0
+  ar.each do |element|
+    count += 1 if element == i
+  end
+  print count, " "
+  i += 1
+end
+
+
+63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33
+
+(The number 1 appears 2 times, etc...):
+
+0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1 0 1 0 0 1 0 0 2 1 0 1 1 1 0 1 0 1 0 2 1 3 2 0 0 2 1 2 1 0 2 2 1 2 1 2 1 1 2 2 0 3 2 1 1 0 1 1 1 0 2 2
