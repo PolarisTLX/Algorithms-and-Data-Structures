5309662b5080c34d77716e2c172eed6deacd6d4c
PolarisTLX <paul.a.rail@gmail.com>
2018-06-14 23:22:54 -0400

    Taking notes on common software design patterns

---
+++ Design Patterns.js
@@ -0,0 +1,5 @@
+Software Design Patterns:
+<---- DONE:
+Learned about the Strategy Pattern and the Observer Pattern,
+---->
+Decorator Pattern,  if you need to have many variations of your class, to avoid class explosion, you instead create decorators that wrap a base case (or another decorator), you can have many variations of your class by wrapping it in many decorators.
