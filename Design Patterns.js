4c5f4346d7e7d874519c63485784311a5409d4f3
Paul Rail <paul.a.rail@gmail.com>
2018-06-21 10:54:51 -0400

    Advice on patterns from veteran programmer

--- Design Patterns.js
+++ Design Patterns.js
@@ -3,3 +3,9 @@ Software Design Patterns:
 Learned about the Strategy Pattern and the Observer Pattern,
 ---->
 Decorator Pattern,  if you need to have many variations of your class, to avoid class explosion, you instead create decorators that wrap a base case (or another decorator), you can have many variations of your class by wrapping it in many decorators.
+
+
+Advice for programming vetera:
+Learn the patterns (Strategy pattern, Visitor Pattern and Factory pattern are all very common), but a warning
+It's a common mistake when people learn these they start applying them everywhere
+These patterns tend to increase the complexity of the design and so need be used selectively
